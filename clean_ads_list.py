ea4b53b01fd4e53146815bb852b098666024e56b
cleggm1 <cleggm1@users.noreply.github.com>
2016-04-28 10:07:54 -0500

    Update clean_ads_list.py

--- clean_ads_list.py
+++ clean_ads_list.py
@@ -2,8 +2,11 @@ import re
 
 input_to_do = open('ads_to_do.in', 'r').readlines()
 input_done = open('ads_done.in', 'r').readlines()
-output = open('/afs/fnal.gov/files/home/room3/cleggm1/clean_ads_to_do.txt', 'w')
-input_to_do = [re.sub(r'\d+\)\s+', '', i) for i in input_to_do]
-input_done = [re.sub(r'\d+\)\s+', '', i) for i in input_done]
-output.write(''.join(list(set(input_to_do) - set(input_done))))
+output = open('/nashome/c/cleggm1/clean_ads_to_do.txt', 'w')
+input_to_do = [re.sub(r'\d+[\.\)]\s+', '', i) for i in input_to_do]
+input_to_do = [i.strip(r'\s+$') for i in input_to_do]
+input_done = [re.sub(r'\d+[\.\)]\s+', '', i) for i in input_done]
+input_done = [i.strip(r'\s+$') for i in input_done]
+output.write(''.join(sorted(list(set(input_to_do) - set(input_done)))))
 output.close()
+print len(list(set(input_to_do) - set(input_done))), 'mismatches to fix'
