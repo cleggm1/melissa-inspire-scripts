c3417a75e236d2db716d030e452954fc8079e110
Melissa Clegg <cleggm1@users.noreply.github.com>
2016-10-26 13:46:38 -0500

    Update new_jobs_mailout.py

--- new_jobs_mailout.py
+++ new_jobs_mailout.py
@@ -1,21 +1,13 @@
-import time
 import datetime
-import sys
 import re
-
-from invenio.search_engine import perform_request_search
-from invenio.search_engine import get_fieldvalues
+from invenio.search_engine import perform_request_search, print_record
 
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-TEST = True
-content = "PLACEHOLDER"
-
-
-def send_jobs_mail(recids)
+def send_jobs_mail(content):
     html= """<html>
 <head></head>
 <body>
@@ -36,7 +28,7 @@ Do not include any other text or type anything in the subject line.<br />
 <p>The INSPIRE HEPJobs database is administered by the Fermilab Library.<br />
 Please send any comments or questions to <a href="mailto:jobs@inspirehep.net">jobs@inspirehep.net</a>.</p>
 """ % content
-
+    print html
     text = BeautifulSoup(html).text
 
     hepjobs_email = '"HEPJobs Database Administrator" <jobs@inspirehep.net>'
@@ -49,8 +41,7 @@ Please send any comments or questions to <a href="mailto:jobs@inspirehep.net">jo
     msg['From'] = hepjobs_email
     msg['To'] = hepjobs_email
 #    msg['BCC'] = 'jobs@fnal.gov'
-    if TEST:
-        msg['BCC'] = 'cleggm1@fnal.gov'
+    msg['BCC'] = 'cleggm1@fnal.gov'
     msg['X-Auto-Response-Suppress'] = 'OOF, DR, RN, NRN'
 
     # Record the MIME types of both parts - text/plain and text/html.
@@ -64,50 +55,57 @@ Please send any comments or questions to <a href="mailto:jobs@inspirehep.net">jo
     msg.attach(part2)
 
     # Send the message via local SMTP server.
-    stmp_email = smtplib.SMTP('localhost')
+#    stmp_email = smtplib.SMTP('localhost')
     # sendmail function takes 3 arguments: sender's address, recipient's address
     # and message to send - here it is sent as one string.
-    stmp_email.sendmail(hepjobs_email, hepjobs_email, msg.as_string())
-    stmp_email.quit()
+#    stmp_email.sendmail(hepjobs_email, hepjobs_email, msg.as_string())
+#    stmp_email.quit()
 
 def main():
-    filename = "tmp_"+__name__+".txt"
-    stored_file = open(filename, 'wr')
-    for line in stored_file.readlines:
+    content = []
+    recids = []
+    date_last_run = False
+    latest_recid = False
+    latest = False
+    filename = re.sub(".py", "_log", "tmp_"+__file__)
+    stored_file = open(filename, 'r+')
+    for line in stored_file.readlines():
         matchObj1 = re.match('most recent recid = (\d+)', line)
         if matchObj1:
             latest_recid = matchObj1.group(0)
-        else:
-            latest_recid = False
         matchObj2 = re.match('date last run = (.*)', line)
         if matchObj2:
             date_last_run = matchObj2.group(0)
-        else:
-            date_last_run = False
-    recids = []
-    latest = False
     if date_last_run and latest_recid:
         results = perform_request_search(p="fin da >= %s" % date_last_run, cc="Jobs")
     else:
         date_last_run = raw_input("""Couldn't find the date of the most recently sent New Jobs Mailout.
-        Send jobs posted on and after this date (yyyy-mm-dd): """
+Send jobs posted on and after this date (yyyy-mm-dd): """)
         if date_last_run:
             results = perform_request_search(p="fin da >= %s" % date_last_run, cc="Jobs")
     if len(results) > 0:
         for r in results:
             recids.append(r)
-        recids = [x for x in recids if x > latest_recid]
-        latest = max(recids)
+        print recids
+        if latest_recid:
+            recids = [x for x in recids if x > latest_recid]
+        print recids
+        if recids:
+            latest = max(recids)
+        else:
+            print "No postings since %s." % date_last_run
 
     else:
-        print "No postings since the last mailout (%s)." % date_last_run
-        break
+        print "No postings since %s." % date_last_run
     if latest:
         stored_file.write("most recent recid = %s" % str(latest))
         today = str(datetime.date.today())
-        stored_file.append("date last run = %s" today)
-        stored_file.append("records in last mailout = %s" % ', '.join(str(x) for x in sorted(recids))
-        send_jobs_mail(recids)
+        stored_file.write("date last run = %s" % today)
+        stored_file.write("records in last mailout = %s" % ', '.join(str(x) for x in sorted(recids)))
+        for r in recids:
+            content.append(print_record(r,ot=[],format='he'))
+        content = ''.join(content)
+        send_jobs_mail(content)
     stored_file.close()
 
 if __name__ == '__main__':
