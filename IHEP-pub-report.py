9a99341ff0401792b196082b2d980ba1d8185be5
Melissa Clegg <cleggm1@users.noreply.github.com>
2017-04-19 15:34:07 +0800

    Create IHEP-pub-report.py

---
+++ IHEP-pub-report.py
@@ -0,0 +1,22 @@
+"""
+Provides text CV output of papers with current citation counts
+"""
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import re
+from invenio.search_engine import perform_request_search
+from invenio.search_engine import print_record
+
+search = raw_input('Search: ')
+file = 'theory_pubs_'+date+'.doc'
+x = perform_request_search(p=search, cc="HEP")
+with open(file, 'w') as output:
+    for r in x:
+      cv = print_record(r, format='htcv')
+      cv = re.sub(r'<br/>', '', cv)
+      cv = re.sub(r'\s\s+', '', cv)
+      citesearch = perform_request_search(p="refersto:recid:%i" % r, cc="HEP")
+      cv = cv + '\n' + len(citesearch) + ' citations'
+      print cv + '\n\n'
+      output.write(cv +'\n\n')
+  
